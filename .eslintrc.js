6b4183815f1b10fecddd90b8b8298db6c94f95bc
Brajesh Anokha <brajesh.anokha@publicissapient.com>
2022-07-11 09:50:31 +0530

    second committ

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    // '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true },
+      { doubleQuote: true },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        // '@typescript-eslint/no-explicit-any': 'off',
-    }
-}
+  },
+};
--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import express,{Request,Response} from "express";
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { createConnection } from "typeorm"
+import { createConnection } from 'typeorm';
 
 const app = express();
 app.use(helmet());
 const allowedOrigins = ['http://localhost:3000'];
 const options: cors.CorsOptions = {
-  origin: allowedOrigins
+  origin: allowedOrigins,
 };
 
 app.use(cors(options));
@@ -16,26 +16,30 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
-
-createConnection().then(()=>{
-console.log('DB Connected')
-}).catch((e)=>{
-    console.log('Error:'+e)
-}) 
+createConnection()
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((e) => {
+    console.log('Error:' + e);
+  });
 
 app.listen(PORT, () => {
-    console.log('Server is up on port - ', PORT);
+  console.log('Server is up on port - ', PORT);
 });
 
 // app.use('/api', misrouter);
 
 app.get('/api/health', (req: Request, res: Response) => {
-  res.json("jnc-mis service health is good.")
+  res.json(`jnc-mis service health is good, and runnubf on port ${PORT}`);
 });
 
 app.use((error: any, req: Request, res: Response, next: any) => {
   console.log('error - ', error);
-  if (error) res.status(500).send({ statusCode: error.statusCode, msg: error.error.msg });
+  if (error)
+    res
+      .status(500)
+      .send({ statusCode: error.statusCode, msg: error.error.msg });
   next();
 });
 
